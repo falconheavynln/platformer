4a26dc6dfb1c467cc409e6ceec47c2800b2110ac
adrian <falconheavynln@gmail.com>
2024-08-16 12:24:19 -0700

    added scrolling blocks, collision, and new player skin

--- platformer.py
+++ platformer.py
@@ -6,22 +6,25 @@ from os.path import isfile, join
 pygame.init()
 
 CAPTION = "thing"
+ANIM_DELAY = 7
 WIDTH = 1500
 HEIGHT = 800
 FPS = 60
 
-MAXSPEED = 20
+MSPEED = 20  # max ground speed
 AGILITY = 5
-JUMP = 25
+JUMP = 1.5
 FRICTION = 4
-GRAVITY = 40
-TERMINALVEL = 180
+GRAVITY = 20
+TERMINALVEL = 180  # max falling speed
+SCROLL_WIDTH = 400  # distance from side of screen to scroll x
 
-ANIM_DELAY = 7
+BLOCKS = [(60 * i, 660, 60, 60) for i in range(20)] + [(120, 480, 60, 60)]
 
 PATH = "assets"
 ICON = "earth_crust.png"
 TILE = "bg_tile_lvl1.png"
+CHARACTER = "square"
 
 wd = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(CAPTION)
@@ -29,8 +32,8 @@ pygame.display.set_icon(pygame.image.load(join(PATH, ICON)))
 
 # use extra player movements if you have time
 
-# pose var combines direction and animation_count,
-# be sure to use turn pose into the animation_count
+# pose var combines direction and animcount,
+# be sure to use turn pose into the animcount
 
 # make a function for meters to pixels, the player
 # is 0.25 meters tall
@@ -38,16 +41,18 @@ pygame.display.set_icon(pygame.image.load(join(PATH, ICON)))
 # x_vel is velocity to the right
 # y_vel is velocity down
 
+# pygame.Surface needs SRCALPHA as 2nd param
 
-def flip_img(sprites):
+
+def flip_image(sprites):
     return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
 
 
 def load_sprite_sheets(path, width, height, direction=False):
-    imgs = [f for f in listdir(path) if isfile(join(path, f))]
+    images = [f for f in listdir(path) if isfile(join(path, f))]
     allsprites = {}
-    for img in imgs:
-        spritesheet = pygame.image.load(join(path, img)).convert_alpha()
+    for image in images:
+        spritesheet = pygame.image.load(join(path, image)).convert_alpha()
         sprites = []
         for i in range(spritesheet.get_width() // width):
             surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
@@ -55,75 +60,36 @@ def load_sprite_sheets(path, width, height, direction=False):
             surface.blit(spritesheet, (0, 0), rect)
             sprites.append(surface)
         if direction:
-            allsprites[img.replace(".png", "") + "_right"] = sprites
-            allsprites[img.replace(".png", "") + "_left"] = flip_img(sprites)
+            allsprites[image.replace(".png", "") + "_right"] = sprites
+            allsprites[image.replace(".png", "") + "_left"] = flip_image(sprites)
         else:
-            allsprites[img.replace(".png", "")] = sprites
+            allsprites[image.replace(".png", "")] = sprites
 
     return allsprites
+
+
+def load_block(w, h):
+    path = join(PATH, "terrain", "block.png")
+    image = pygame.image.load(path).convert_alpha()
+    surface = pygame.Surface((w, h), pygame.SRCALPHA, 32)
+    rect = pygame.Rect(0, 0, w, h)  # (0, 0) is top-left coords. w, h is dimensions
+    surface.blit(image, (0, 0), rect)
+    return surface
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
         super().__init__()
-        self.SPRITES = load_sprite_sheets(join(PATH, "squid_guy"), 60, 60, True)
-        print(self.SPRITES)
+        self.SPRITES = load_sprite_sheets(join(PATH, CHARACTER), 60, 60, True)
         self.rect = pygame.Rect(x, y, w, h)
-        self.xvel = 0
-        self.yvel = 0
-        self.xpos = x
-        self.ypos = y
-        self.size = 60
-        self.grounded = False
-        self.mask = None
-        self.pose = "left"
-        self.fallcount = 0
-        self.animation_count = 0
-
-    def m_move(self, xdis, ydis):
-        self.xpos += xdis
-        self.ypos += ydis
-
-    def m_left(self):
-        if self.pose != "left":
-            self.pose = "left"
-            self.animation_count = 0
-
-    def m_right(self):
-        if self.pose != "right":
-            self.pose = "right"
-            self.animation_count = 0
-
-    def keylink(self):
-        keys = pygame.key.get_pressed()
-        self.x_vel = 0
-        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-            self.m_left()
-            if self.xvel <= -MAXSPEED + AGILITY:
-                self.xvel = -MAXSPEED
-            else:
-                self.xvel -= AGILITY
-        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-            self.m_right()
-            if self.xvel >= MAXSPEED - AGILITY:
-                self.xvel = MAXSPEED
-            else:
-                self.xvel += AGILITY
-        if (
-            keys[pygame.K_SPACE] or keys[pygame.K_UP] or keys[pygame.K_w]
-        ) and self.ypos == 600:
-            self.yvel -= JUMP
+        self.xvel, self.yvel, self.size = 0, 0, 60
+        self.mask, self.pose = None, "right"
+        self.fallcount, self.jumpcount, self.animcount = 0, 0, 0
 
     def loop(self, fps):
-        if self.ypos < 600 and (self.ypos + self.yvel < 600):
-            self.fallcount += 1
-            self.yvel += (self.fallcount / fps) * GRAVITY  # gravity
-        else:
-            self.yvel = 0
-            self.ypos = 600
-            self.fallcount = 0
-        self.keylink()
-        self.m_move(self.xvel, self.yvel)
+        self.yvel += (self.fallcount / fps) * GRAVITY  # gravity
+        self.rect.x += self.xvel
+        self.rect.y += self.yvel
         # friction
         if self.xvel > 0 + FRICTION / 2:
             self.xvel -= FRICTION
@@ -131,46 +97,119 @@ class Player(pygame.sprite.Sprite):
             self.xvel += FRICTION
         else:
             self.xvel = 0
+        self.fallcount += 1
         self.update_sprite()
 
     def update_sprite(self):
         sprite_sheet = "idle"
+        if self.yvel < 0:
+            if self.jumpcount == 1:
+                sprite_sheet = "jump"
+            elif self.jumpcount == 2:
+                sprite_sheet = "double_jump"
+        elif self.yvel > GRAVITY * 2:
+            sprite_sheet = "fall"
         if self.xvel != 0:
             sprite_sheet = "run"
-
         sprite_sheet_name = sprite_sheet + "_" + self.pose
         sprites = self.SPRITES[sprite_sheet_name]
-        sprite_index = (self.animation_count // ANIM_DELAY) % len(sprites)
+        sprite_index = (self.animcount // ANIM_DELAY) % len(sprites)
         self.sprite = sprites[sprite_index]
-        self.animation_count += 1
-        self.update()
-
-    def update(self):
+        self.animcount += 1
         self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
         self.mask = pygame.mask.from_surface(self.sprite)
 
-    def draw(self, wd):
-        wd.blit(self.sprite, (self.xpos, self.ypos))
+    def draw(self, wd, offset_x):
+        wd.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
 
-def draw(wd, player, TILE):
-    tile_img = pygame.image.load(join(PATH, TILE))
-    _, _, tile_width, tile_height = tile_img.get_rect()
+class Object(pygame.sprite.Sprite):
+    def __init__(self, x, y, w, h, name=None):
+        super().__init__()
+        self.rect = pygame.Rect(x, y, w, h)
+        self.image = pygame.Surface((w, h), pygame.SRCALPHA)
+        self.w, self.h = w, h
+        self.name = name
+
+    def draw(self, wd, offset_x):
+        wd.blit(self.image, (self.rect.x - offset_x, self.rect.y))
+
+
+class Block(Object):
+    def __init__(self, x, y, w, h):
+        super().__init__(x, y, w, h)
+        block = load_block(w, h)
+        self.image.blit(block, (0, 0))
+        self.mask = pygame.mask.from_surface(self.image)
+
+
+def keys(player, blocks):
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        if player.pose != "left":
+            player.pose = "left"
+            player.animcount = 0
+        player.xvel = (
+            -MSPEED if player.xvel <= -MSPEED + AGILITY else player.xvel - AGILITY
+        )
+    elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        if player.pose != "right":
+            player.pose = "right"
+            player.animcount = 0
+        player.xvel = (
+            MSPEED if player.xvel >= MSPEED - AGILITY else player.xvel + AGILITY
+        )
+    if (
+        (keys[pygame.K_SPACE] or keys[pygame.K_UP] or keys[pygame.K_w])
+        and player.fallcount == 1
+        and player.jumpcount < 2
+    ):
+        player.yvel = -GRAVITY * JUMP
+        player.animcount, player.jumpcount = 0, player.jumpcount + 1
+        player.fallcount = 0 if player.jumpcount == 1 else player.fallcount
+    vertical_collision(player, blocks)
+
+
+def vertical_collision(player, objects):
+    collided_objects = []
+    for object in objects:
+        if pygame.sprite.collide_mask(player, object):
+            if player.yvel > 0:
+                player.rect.bottom = object.rect.top
+                player.fallcount, player.yvel, player.jumpcount = 0, 0, 0
+            elif player.yvel < 0:
+                player.rect.top = object.rect.bottom
+                player.count, player.yvel = 0, -player.yvel // 2
+            collided_objects.append(object)
+    return collided_objects
+
+
+def draw(wd, player, TILE, objects, offset_x):
+    # background
+    tile_image = pygame.image.load(join(PATH, TILE))
+    _, _, tile_width, tile_height = tile_image.get_rect()
     tile_pos = []
     for i in range(WIDTH // tile_width + 10):
         for j in range(HEIGHT // tile_height + 10):
             pos = (i * tile_width, j * tile_height)
             tile_pos.append(pos)
     for TILE in tile_pos:
-        wd.blit(tile_img, TILE)
-    pygame.draw.rect(wd, (0, 0, 200), [0, 600 + player.size, 10000, 1000])
-    player.draw(wd)
+        wd.blit(tile_image, TILE)
+
+    for object in objects:
+        object.draw(wd, offset_x)
+
+    player.draw(wd, offset_x)
     pygame.display.update()
 
 
 def main(wd):
     clock = pygame.time.Clock()
-    player = Player(100, 100, 50, 50)
+    player = Player(530, 100, 50, 50)
+    blocks = [Block(i[0], i[1], i[2], i[3]) for i in BLOCKS]
+    player.loop(FPS)
+    offset_x = 0
+
     run = True
     while run:
         clock.tick(FPS)
@@ -180,7 +219,13 @@ def main(wd):
                 break
 
         player.loop(FPS)
-        draw(wd, player, TILE)
+        keys(player, blocks)
+        draw(wd, player, TILE, blocks, offset_x)
+
+        if (
+            player.rect.right - offset_x >= WIDTH - SCROLL_WIDTH and player.xvel > 0
+        ) or ((player.rect.left - offset_x <= SCROLL_WIDTH) and player.xvel < 0):
+            offset_x += player.xvel
     pygame.quit()
     quit()
 
